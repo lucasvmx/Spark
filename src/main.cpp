273dfb9613c9c4b775e45b025c4913f3a0cccc53
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-02-19 23:05:17 -0300

    feat: :sparkles: English language is showed by default on UI refactor: :hammer: add macro to translate strings
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include <QApplication>
 #include <QMessageBox>
 #include <QObject>
+#include <QTranslator>
 #include <signal.h>
 #ifdef Q_OS_WIN
 #include <windows.h>
@@ -20,14 +21,16 @@
 #include "wzhack.h"
 #include "dynamic_loader.h"
 
+#define TR(x)   QObject::tr(x)
+
 void signal_handler(int signum)
 {
     switch(signum)
     {
     case SIGABRT:
     case SIGSEGV:
-        QMessageBox::critical(0, QObject::tr("Falha de aplicativo"),
-                              QObject::tr("Um erro crítico foi detectado e o programa precisa ser fechado. "
+        QMessageBox::critical(0, TR("Falha de aplicativo"),
+                              TR("Um erro crítico foi detectado e o programa precisa ser fechado. "
                                           "Código do erro: %1").arg(signum));
     }
 
@@ -46,13 +49,14 @@ static void CheckForOtherInstance()
     hMutex = CreateMutexA(nullptr, TRUE, "Spark_Mutex");
     if (hMutex == nullptr)
     {
-        QMessageBox::critical(nullptr, "Error", QString::asprintf("Failed to create mutex: %lu", GetLastError()));
+        QMessageBox::critical(nullptr, TR("Erro"), QString::asprintf(TR("Falha ao criar mutex: %lu").toStdString().c_str(), GetLastError()));
         exit(1);
     }
 
     if (GetLastError() == ERROR_ALREADY_EXISTS)
     {
-        QMessageBox::critical(nullptr, "Error", "Outra instância já está em execução");
+        QMessageBox::critical(nullptr, TR("Erro"),
+                              TR("Outra instância já está em execução"));
         exit(1);
     }
 #else
@@ -63,18 +67,23 @@ static void CheckForOtherInstance()
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    frmMain mainForm;
+    frmMain *mainForm;
+    QTranslator translator;
 
     signal(SIGABRT, signal_handler);
     signal(SIGSEGV, signal_handler);
+
+    translator.load("spark_en_us");
+    app.installTranslator(&translator);
 
     CheckForOtherInstance();
 
     // Carrega as funcões da DLL
     LoadDLLFunctions();
 
+    mainForm = new frmMain();
     // Exibe a janela
-    mainForm.show();
+    mainForm->show();
 
     return app.exec();
 }
