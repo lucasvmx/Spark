077c5ecb0f44c65b203b7e9b624fa4e77f44dbd2
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-04-03 14:56:58 -0300

    chore: :zap: remove obsolete functions
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- src/dynamic_loader.h
+++ src/dynamic_loader.h
@@ -38,13 +38,6 @@ typedef DWORD   (WINAPI *GFVIS)(LPCSTR lptstrFilename, LPDWORD lpdwHandle);
 typedef BOOL    (WINAPI *VQV)(LPCVOID pBlock, LPCSTR lpSubBlock, LPVOID *lplpBuffer, PUINT puLen);
 typedef BOOL    (WINAPI *GMI)(HANDLE hProcess, HMODULE hModule, LPMODULEINFO lpmodinfo, DWORD cb);
 
-// Funções do warzone 2100
-typedef void (*pClear)();
-typedef void (*pAddPower)(int player, int32_t quantity);
-typedef bool (*pIsHumanPlayer)(int player);
-typedef void (*pPrintChatMsg)(const char *text, int from, bool team);
-typedef void (*pSetPowerModifier)(int player, int modifier);
-
 /**
  * @brief Alias para GetFileVersionInfoA
  */
@@ -69,32 +62,6 @@ extern GMI fGetModuleInformation;
  * @brief fQueryFullProcessImageFileName Alias para QueryFullProcessImageFileName
  */
 extern QFPINA fQueryFullProcessImageFileName;
-
-/**
- * @brief addPower Aumenta a energia do jogador (player, quantity)
- */
-extern pAddPower addPower;
-
-/**
- * @brief printchatmsg Exibe uma mensagem de texto (const char *text, int from, bool team);
- */
-extern pPrintChatMsg printchatmsg;
-
-/**
- * @brief setPowerModifier Aumenta a porcentagem de energia gerada por cada extrator de petróleo
- * (int player, int modifier)
- */
-extern pSetPowerModifier setPowerModifier;
-
-/**
- * @brief isHumanPlayer Verifica se o jogador é humano (int player)
- */
-extern pIsHumanPlayer IsHumanPlayer;
-
-/**
- * @brief ClearPlayerPower Zera a energia do jogador selecionado
- */
-extern pClear ClearPlayerPower;
 
 // Funções comuns
 
