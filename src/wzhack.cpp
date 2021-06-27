598b932ac24e1a490b2d154bddb8865c48905c94
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-06-26 23:19:33 -0300

    feat: :sparkles: add support for version 4.0.1
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- src/wzhack.cpp
+++ src/wzhack.cpp
@@ -116,7 +116,7 @@ unsigned short WzHack_GetWarzoneVersion(const char *wz_filename)
     // Compila o número de versão para um único número
     iversion = (major * 100) + (minor * 10) + patch;
 
-    if((iversion > WZ_400) || (iversion < WZ_239))
+    if((iversion > WZ_401) || (iversion < WZ_239))
 		iversion = WZ_UNKNOWN;
 
 	if (buf)
@@ -127,7 +127,7 @@ unsigned short WzHack_GetWarzoneVersion(const char *wz_filename)
 
 BOOL WzHack_GetStructStartIndex(int wz_version, unsigned short *start)
 {
-    unsigned short local_start;
+    unsigned short local_start = 0;
 	BOOL bOk = FALSE;
     const size_t len = arraySize(player_info);
 
@@ -404,6 +404,7 @@ uint32_t GetWarzoneMaxPowerValue(int warzone_version)
     case WZ_340:
     case WZ_341:
     case WZ_400:
+    case WZ_401:
              return 1000000;
     }
 
