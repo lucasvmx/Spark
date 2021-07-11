8e37a8f8614fbb9138bc5fdbd87d33e376e75c4a
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-07-11 15:15:54 -0300

    chore: add input to get warzone version
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- tools/hextool.py
+++ tools/hextool.py
@@ -5,10 +5,11 @@
 
 first = int(input('first address: '), 16)
 second = int(input('second address: '), 16)
+ver = input('Warzone version: ')
 diff = second - first
 current = first
 
 print(f'offset is {diff} or {hex(diff)} in base 16')
 for i in range(0, 11):
-		print('{%d, 0, 0, 0, %s, 0, 0, 0, 0, 0, WZ_401},' % (i, hex(current)))
+		print('{%d, 0, 0, 0, %s, 0, 0, 0, 0, 0, %s},' % (i, hex(current), ver))
 		current += diff
